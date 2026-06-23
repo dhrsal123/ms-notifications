986555ba36d7becf97001d237bac414347ed593f
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-23 12:42:04 -0400

    feat: improve template engine implementation

--- src/main/java/io/cinema/msnotifications/strategy/impl/NotificationProviderStrategyImpl.java
+++ src/main/java/io/cinema/msnotifications/strategy/impl/NotificationProviderStrategyImpl.java
@@ -6,7 +6,7 @@ import io.cinema.msnotifications.domain.dto.NotificationDTO;
 import io.cinema.msnotifications.domain.enumerated.NotificationProvider;
 import io.cinema.msnotifications.service.EmailService;
 import io.cinema.msnotifications.strategy.NotificationProviderStrategy;
-import io.cinema.msnotifications.util.TemplateUtil;
+import io.cinema.msnotifications.util.TemplateEngine;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class NotificationProviderStrategyImpl implements NotificationProviderStrategy {
+    private final TemplateEngine templateEngine;
     private final EmailService emailService;
 
     @Override
@@ -27,7 +28,7 @@ public class NotificationProviderStrategyImpl implements NotificationProviderStr
         log.info("Sending notification via GOOGLE to: {}", notification.recipient());
 
         String template = notification.notificationType() + ".mustache";
-        String message = TemplateUtil.buildTemplate(template, notification.templateModel());
+        String message = templateEngine.buildTemplate(template, notification.templateModel());
 
         try {
             emailService.sendEmail(notification.recipient(), notification.subject(), message);
---
+++ src/main/java/io/cinema/msnotifications/util/TemplateEngine.java
@@ -0,0 +1,53 @@
+package io.cinema.msnotifications.util;
+
+import com.samskivert.mustache.Mustache;
+import io.cinema.domain.exceptions.CinemaException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import static io.cinema.domain.enumerated.CinemaExceptionTypes.TECHNICAL_ERROR;
+
+@Component
+@RequiredArgsConstructor
+public class TemplateEngine {
+    private static final String TEMPLATES_BASE_PATH = "templates/";
+    private final ResourceLoader resourceLoader;
+    private final Mustache.Compiler mustacheCompiler;
+
+    public String buildTemplate(
+            String templateName,
+            Map<String, Object> variables
+    ) {
+        var cleanName = templateName.startsWith("/") ? templateName.substring(1) : templateName;
+
+        Resource resource = resourceLoader.getResource(TEMPLATES_BASE_PATH + cleanName);
+
+        if (!resource.exists()) {
+            throw new IllegalArgumentException("Template file not found at path: " + TEMPLATES_BASE_PATH + cleanName);
+        }
+
+        StringWriter writer = new StringWriter();
+
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+            var template = mustacheCompiler
+                    .compile(reader);
+
+            template.execute(variables, writer);
+
+            return writer.toString();
+        } catch (IOException e) {
+            throw new CinemaException(
+                    "Error reading template: " + templateName + " Error: " + e.getMessage(), TECHNICAL_ERROR
+            );
+        }
+    }
+}
