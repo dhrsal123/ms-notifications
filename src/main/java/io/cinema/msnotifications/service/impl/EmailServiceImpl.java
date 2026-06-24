41973a6b9349fb464bb8c34f50e5eebdb7a1e3a2
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-24 14:25:02 -0500

    feat: update email provider implementation so that it doesnt require delegate

--- src/main/java/io/cinema/msnotifications/service/impl/EmailServiceImpl.java
+++ src/main/java/io/cinema/msnotifications/service/impl/EmailServiceImpl.java
@@ -1,18 +1,11 @@
 package io.cinema.msnotifications.service.impl;
 
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.Message;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.auth.oauth2.GoogleCredentials;
 import io.cinema.domain.enumerated.CinemaExceptionTypes;
 import io.cinema.domain.exceptions.CinemaException;
 import io.cinema.msnotifications.config.EmailProperties;
 import io.cinema.msnotifications.service.EmailService;
-import jakarta.mail.MessagingException;
 import jakarta.mail.Session;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
@@ -21,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Base64;
 import java.util.Properties;
 
@@ -29,60 +21,39 @@ import java.util.Properties;
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
-    private static final String APPLICATION_NAME = "cinema-notifications";
+    private final Gmail gmail;
     private final EmailProperties emailProperties;
 
     @Override
-    public void sendEmail(
-            String to,
-            String subject,
-            String body
-    ) throws MessagingException, IOException {
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            var props = new Properties();
+            var session = Session.getDefaultInstance(props, null);
 
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-                .createScoped(GmailScopes.GMAIL_SEND)
-                .createDelegated(emailProperties.getSenderEmail());
+            var msg = new MimeMessage(session);
 
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+            msg.setFrom(new InternetAddress(emailProperties.getSenderEmail()));
+            msg.setSubject(subject, "UTF-8");
+            msg.addRecipient(jakarta.mail.Message.RecipientType.TO, new InternetAddress(to));
+            msg.setContent(body, "text/html; charset=utf-8");
 
-        Gmail service = new Gmail.Builder(
-                new NetHttpTransport(),
-                GsonFactory.getDefaultInstance(),
-                requestInitializer
-        )
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+            try (ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
+                msg.writeTo(buffer);
+                String encodedMessage = Base64
+                        .getUrlEncoder()
+                        .withoutPadding()
+                        .encodeToString(buffer.toByteArray());
 
-        var props = new Properties();
-        var session = Session.getDefaultInstance(props, null);
+                var message = new Message();
+                message.setRaw(encodedMessage);
 
-        var msg = new MimeMessage(session);
-        msg.setSubject(subject);
-        msg.addRecipient(jakarta.mail.Message.RecipientType.TO, new InternetAddress(to));
-        msg.setContent(body, "text/html");
+                message = gmail.users().messages().send("me", message).execute();
 
-        try (ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
-            msg.writeTo(buffer);
-            byte[] messageBytes = buffer.toByteArray();
-            var encodedMessage = Base64
-                    .getEncoder()
-                    .encodeToString(messageBytes);
-
-            var message = new Message();
-            message.setRaw(encodedMessage);
-
-            message = service
-                    .users()
-                    .messages()
-                    .send("me", message)
-                    .execute();
-
-            log.info("Message sent to {}", to);
-            log.info("Message id: {}", message.getId());
+                log.info("Message sent to {}", to);
+                log.info("Message id: {}", message.getId());
+            }
         } catch (Exception e) {
             throw new CinemaException("Something went wrong: " + e.getMessage(), CinemaExceptionTypes.TECHNICAL_ERROR);
         }
-
-
     }
 }
